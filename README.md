96e3dc25154b4a230c018ed49eecc622f4bea228
avukov02 <avukov02@fesb.hr>
2021-03-20 19:52:35 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learning C#
+
+<img alt="C#" src="https://img.shields.io/badge/c%23%20-%23239120.svg?&style=for-the-badge&logo=c-sharp&logoColor=white"/>
+
+I followed Bruce Van Horn's C# tutorial and made simple app.
---
+++ SchoolFormsApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SchoolLibrary;
+
+namespace SchoolFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        private void btnPushToTest_Click_1(object sender, EventArgs e)
+        {
+            var testSchool = new School();
+            testSchool.Name = textName.Text;
+            testSchool.City = textCity.Text;
+            testSchool.State = textState.Text;
+            testSchool.Zip = textZip.Text;
+            testSchool.PhoneNumber = textPhone.Text;
+            testSchool.Address = textAddress.Text;
+            try
+            {
+                testSchool.TwitterAddress = textTwitter.Text;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show(testSchool.ToString());
+
+            var teacher = new Teacher();
+        }
+
+        private void btnTestTeacher_Click(object sender, EventArgs e)
+        {
+            var teacher = new Teacher();
+            var gp = teacher.ComputeGradeAverage();
+            MessageBox.Show("the teacher grade average is "+gp);
+        }
+
+        private void btnTestStudent_Click(object sender, EventArgs e)
+        {
+            var student = new Student();
+            var gp = student.ComputeGradeAverage();
+            MessageBox.Show("the student grade average is " + gp);
+        }
+    }
+}
